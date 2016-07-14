e62124c531b63580a6e6e5014a671bc2e6ae9b7c
wuwei <wuwei@baomap.com>
2016-07-14 17:02:46 +0800

    modify init.el

--- init.el
+++ init.el
@@ -149,8 +149,7 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(monokai
-                         spacemacs-dark
+   dotspacemacs-themes '(spacemacs-dark
                          occidental
                          spacemacs-light
                          solarized-light
